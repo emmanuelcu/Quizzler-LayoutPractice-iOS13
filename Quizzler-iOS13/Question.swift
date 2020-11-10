1eb136ac78de34da7f8097d67c0e93ca0b7aa64c
Emmanuel Zoolander <12421140+emmanuelcu@users.noreply.github.com>
2020-11-09 19:53:32 -0600

    The structure Question was created where Question { let text: String let answer:Bool}

---
+++ Quizzler-iOS13/Question.swift
@@ -0,0 +1,19 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Emmanuel Cuevas on 09/11/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+    
+    init(q: String, a: String) {
+        text = q
+        answer = a
+    }
+}
